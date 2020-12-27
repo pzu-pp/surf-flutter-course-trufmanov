4f53bdc35f232b191b461fd8853c0e650f6a523c
Aleksey <pzu-pp@yandex.ru>
2020-12-27 21:28:56 +0300

    3-4-MyFirstWidget

--- lib/main.dart
+++ lib/main.dart
@@ -26,92 +26,55 @@ class MyApp extends StatelessWidget {
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+//      home: MyFirstWidget_Stateless(),
+      home: MyFirstWidget_Stateful(),
+
+//      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
+class MyFirstWidget_Stateless extends StatelessWidget {
   int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    _counter++;
+    print("Stateless widget build counter = $_counter");
+    // I/flutter ( 9920): Stateless widget build counter = 1
+    // Stateless widget пересоздается при каждой отрисовке экрана (пересоздается
+    // все дерево виджетом) - каждый раз это новый объект, build вызывается его
+    // конструктором однократно.
+    return Container(
+      child: Center(
+        child: Text("Hello"),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+    );
+  }
+}
+
+class MyFirstWidget_Stateful extends StatefulWidget {
+  @override
+  _MyFirstWidget_StatefulState createState() => _MyFirstWidget_StatefulState();
+}
+
+class _MyFirstWidget_StatefulState extends State<MyFirstWidget_Stateful> {
+  int _counter = 0;
+  @override
+  Widget build(BuildContext context) {
+    _counter++;
+    print("Stateful widget build counter = $_counter"); // always shows 1, since widget re-builds
+    // I/flutter ( 9920): Stateful widget build counter = 1
+    // I/flutter ( 9920): Stateful widget build counter = 2
+    // I/flutter ( 9920): Stateful widget build counter = 3
+    // Непосредственно Stateful widget так же пересоздается при каждой отрисовке
+    // экрана (вместе со всем деревом виджетов). Но он использует объект State
+    // (при перерисовке вызывае его метод Build, а не собственный).
+    // Время жизни State объекта не связано со временем жизни самого виджета.
+    return Container(
+      child: Center(
+        child: Text("Hello"),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
