434f0411c1257a19c000a77f804f5c7a003d1caf
Aleksey <pzu-pp@yandex.ru>
2021-01-13 23:19:25 +0300

    4-1-Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:places/ui/screen/sight_list_screen.dart';
 
 void main() {
   runApp(App());
@@ -9,7 +10,8 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Test app title...',
-      home: MyFirstWidget_Stateless(),
+      home: SightListScreen(),
+//      home: MyFirstWidget_Stateless(),
     );
   }
 }
