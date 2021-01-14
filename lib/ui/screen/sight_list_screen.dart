a96b247c7d7ac510d21c50ae7886fe5add26c9eb
Aleksey <pzu-pp@yandex.ru>
2021-01-14 23:05:13 +0300

    4-2-1-Text

--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -8,6 +8,24 @@ class SightListScreen extends StatefulWidget {
 class _SightListScreenState extends State<SightListScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        //backgroundColor: Colors.white,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        toolbarHeight: 200,
+        title: Text(
+          'Список\nинтересных мест',
+          textAlign: TextAlign.left,
+          maxLines: 2,
+          style: TextStyle(
+            color: Colors.black,
+            fontFamily: 'Roboto-Regular.ttf',
+            fontSize: 32,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      )
+    );
   }
 }
