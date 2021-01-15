66e07d14bfd8ba81cdae0308d5da40760fed9c78
Aleksey <pzu-pp@yandex.ru>
2021-01-15 21:38:46 +0300

    4-3-1-SightMocks

---
+++ lib/domain/sight.dart
@@ -0,0 +1,19 @@
+class Sight {
+
+  String name;
+  double lat;
+  double lon;
+  String url;
+  String details;
+  String type;
+
+  Sight({
+    this.name,
+    this.lat,
+    this.lon,
+    this.url,
+    this.details,
+    this.type,
+  });
+
+}
