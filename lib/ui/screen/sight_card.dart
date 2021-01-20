9675a63a67170c1e4ae96ed974c7c35069df399f
Aleksey <pzu-pp@yandex.ru>
2021-01-20 20:53:38 +0300

    4-5-1-ContentWidgets-SightCard

--- lib/ui/screen/sight_card.dart
+++ lib/ui/screen/sight_card.dart
@@ -10,73 +10,79 @@ class SightCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return AspectRatio(
       aspectRatio: 3 / 2,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Stack(
+      child: Padding(
+        padding: EdgeInsets.all(10.0),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(15.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              Container(
-                height: 100,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(20),
-                      topRight: Radius.circular(20)),
-                  color: Colors.cyan,
-                ),
-              ),
-              Positioned(
-                top: 16,
-                left: 16,
-                child: Text(
-                  sight.type,
-                  style: TextStyle(
+              Stack(
+                children: [
+                  Image.network(
+                    sight.url,
+                    height: 100,
+                    width: double.infinity,
+                    fit: BoxFit.fitWidth,
+                    loadingBuilder: (context, child, loadingProgress) {
+                      if (loadingProgress == null) {
+                        return child;
+                      } else {
+                        return Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      }
+                    },
+                  ),
+                  Positioned(
+                    top: 16,
+                    left: 16,
+                    child: Text(
+                      sight.type,
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: "Roboto",
+                          fontWeight: FontWeight.bold,
+                          fontSize: 14),
+                    ),
+                  ),
+                  Positioned(
+                    top: 18,
+                    right: 19,
+                    child: Container(
                       color: Colors.white,
+                      height: 18,
+                      width: 20,
+                    ),
+                  )
+                ],
+              ),
+              SizedBox(height: 10),
+              Text(
+                sight.name,
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+                style: TextStyle(
+                    fontFamily: "Roboto",
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16),
+              ),
+              SizedBox(height: 2),
+              Expanded(
+                child: Text(
+                  sight.details,
+                  maxLines: 6,
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(
+                      color: Color(0xFF7C7E92),
                       fontFamily: "Roboto",
-                      fontWeight: FontWeight.bold,
                       fontSize: 14),
                 ),
               ),
-              Positioned(
-                top: 18,
-                right: 19,
-                child: Container(
-                  color: Colors.white,
-                  height: 18,
-                  width: 20,
-                ),
-              )
+              SizedBox(height: 10),
             ],
           ),
-          SizedBox(height: 10),
-          Text(
-            sight.name,
-            maxLines: 2,
-            overflow: TextOverflow.ellipsis,
-            style: TextStyle(
-                fontFamily: "Roboto",
-                fontWeight: FontWeight.bold,
-                fontSize: 16),
-          ),
-          SizedBox(height: 2),
-          Expanded(
-            // child: FractionallySizedBox(
-            //   widthFactor: 0.5,
-            // child: ConstrainedBox(
-            //   constraints: BoxConstraints(
-            //       maxWidth: MediaQuery.of(context).size.width / 2),
-              child: Text(
-                sight.details,
-                // maxLines: 3,
-                // overflow: TextOverflow.ellipsis,
-                style: TextStyle(
-                    color: Color(0xFF7C7E92),
-                    fontFamily: "Roboto",
-                    fontSize: 14),
-              ),
-//            ),
-          ),
-          SizedBox(height: 10),
-        ],
+        ),
       ),
     );
   }
