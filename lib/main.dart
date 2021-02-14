51593936675525937085c8af4bfa654d90fe27af
Aleksey <pzu-pp@yandex.ru>
2021-02-14 08:48:07 +0300

    5-1-3-SettingsScreen

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:places/ui/screen/sight_details_screen.dart';
 import 'package:places/ui/screen/visiting_screen.dart';
 import 'package:places/mocks.dart';
 import 'package:places/ui/screen/res/themes.dart';
+import 'package:places/ui/screen/filters_screen.dart';
+import 'package:places/ui/screen/settings_screen.dart';
 
 void main() => runApp(App());
 
@@ -15,15 +17,18 @@ class App extends StatefulWidget {
 
 class _AppState extends State<App> {
   ThemeType _themeType = ThemeType.light;
+//  ThemeType _themeType = ThemeType.dark;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Интересные места',
       theme: _themeType == ThemeType.light ? lightTheme : darkTheme,
+      home: SettingsScreen(onThemeChange: (value) => setState( () => _themeType=value)),
+//      home: FiltersScreen(),
 //      home: VisitingScreen(),
 //      home: SightListScreen(),
-      home: SightDetailsScreen(sight: mocks[4]),
+//      home: SightDetailsScreen(sight: mocks[4]),
     );
   }
 }
