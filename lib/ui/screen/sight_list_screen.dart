e6de50881bc04baaed664d4e1fb207caff121be7
Aleksey <pzu-pp@yandex.ru>
2021-02-25 22:37:55 +0300

    5-2-2-SightSearchScreen

--- lib/ui/screen/sight_list_screen.dart
+++ lib/ui/screen/sight_list_screen.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:places/ui/screen/filters_screen.dart';
 import 'package:places/ui/widgets/sight_card.dart';
 import 'package:places/mocks.dart';
 import 'package:places/ui/widgets/app_bottom_navigation_bar.dart';
 import 'package:places/ui/screen/add_sight_screen.dart';
+import 'package:places/ui/widgets/search_text_field.dart';
+import 'package:places/ui/screen/sight_search_screen.dart';
 
 /// Класс виджета списка достопримечательностей
 class SightListScreen extends StatefulWidget {
@@ -15,8 +18,62 @@ class _SightListScreenState extends State<SightListScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        toolbarHeight: 108,
+        toolbarHeight: 118,
         title: Text('Список интересных мест'),
+        bottom: PreferredSize(
+          preferredSize: Size(double.infinity, 52),
+          child: InkWell(
+            onTap: () => Navigator.of(context).push(
+                MaterialPageRoute(builder: (context) => SightSearchScreen())),
+            child: Container(
+              height: 55,
+              margin: EdgeInsets.only(left: 16, right: 16),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(25)),
+                color: Colors.black12,
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 16, right: 16),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.search,
+                      size: 30,
+                      color: Colors.grey,
+                    ),
+                    Expanded(
+                      child: Text(
+                        'Поиск',
+                        style: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                    TextButton(
+                      child: Image.asset(
+                        'res/Filter.png',
+                        fit: BoxFit.fitWidth,
+                        height: 30,
+                        width: 30,
+                        color: Colors.green,
+                      ),
+                      onPressed: () => Navigator.of(context).push(
+                          MaterialPageRoute(builder: (context) => FiltersScreen())),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+
+////////////////////////////////////////////////////////////////////////////////
+          // InkWell(
+          //     onTap: () => Navigator.of(context).push(
+          //         MaterialPageRoute(builder: (context) => SightSearchScreen())),
+          //     child: SearchTextField(),
+          // ),
+        ),
       ),
       body: SingleChildScrollView(
         child: Column(
@@ -40,7 +97,8 @@ class _SightListScreenState extends State<SightListScreen> {
           ),
           child: InkWell(
             onTap: () {
-              Navigator.of(context).push( MaterialPageRoute( builder: (context) => AddSightScreen()));
+              Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) => AddSightScreen()));
             },
             child: Center(
               child: Row(
